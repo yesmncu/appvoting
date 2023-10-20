5b3b82dce39292214352ebcf6dc91d351158a61d
yesmncu <yunusemresomuncu6@gmail.com>
2023-10-20 16:21:02 +0300

    env added

--- api/store.js
+++ api/store.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://6527f3b1931d71583df1a378.mockapi.io';
+const API_URL = process.env.API_URL;
 
 const contentApi = async () => {
   try {
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['cloudflare-ipfs.com'],
       },
+      env: {
+        API_URL: "https://6527f3b1931d71583df1a378.mockapi.io"
+      }
 }
 
 module.exports = nextConfig
