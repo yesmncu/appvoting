17806aec0b233b41d9afe82614269ea74e615166
yesmncu <yunusemresomuncu6@gmail.com>
2023-10-23 14:24:48 +0300

    minor fix

--- pages/not-found.js
+++ pages/not-found.js
@@ -5,13 +5,13 @@ const NotFound = () => {
   return (
     <>
       <Head>
-        <title> Sayfa Bulunamadı </title>
+        <title>Sayfa Bulunamadı</title>
       </Head>
       <div className='notFound'>
         <div className='container'>
-          <p>Üzgünüz, aradığınız sayfa mevcut değil.</p>
-          <Link href="/" className="btn-default">
-            Anasayfa'ya dön
+          <p> Üzgünüz, aradiginiz sayfa mevcut değil. </p>
+          <Link href="/">
+            <button className="btn-default"> Anasayfa </button>
           </Link>
         </div>
       </div>
